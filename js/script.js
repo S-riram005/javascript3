7625aa2150c2e7431179146b0930db5914b23342
S-riram005 <sriramrajkumar87@gmail.com>
2025-07-23 09:52:51 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-alert("welcome to javascript for external file")
+//alert("welcome to javascript for external file")
+
+
+console.log("welcome to sriram");
+console.log(123456);
+console.log(18.25);
+console.log(true);
+console.log([58,78,96,35]);
+console.log({fname:'sriram',age:25});
+console.table({fname:'sriram',age:25});
+console.error("custom sample error");
+console.warn("custom sample error");
+console.clear();
+console.log("Timer");
+for(i=0;i<10;i++)
+{
+    console.log(i);
+}
+console.timeEnd("Timer");
