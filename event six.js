636f43695c3a83182cd9b1d7f23f0d3599c12c75
S-riram005 <sriramrajkumar87@gmail.com>
2025-08-20 17:18:33 +0530

    first commit

---
+++ event six.js
@@ -0,0 +1,24 @@
+const touchArea=document.getElementById("touchArea");
+touchArea.addEventListener("touchstart",function(e){
+    e.preventDefault
+    touchArea.style.backgroundColor="blue";
+    touchArea.textContent="Touch Started!";
+});
+
+touchArea.addEventListener("touchmove",function(e){
+    e.preventDefault
+    touchArea.style.backgroundColor="green";
+    touchArea.textContent="Touch Moved!";
+});
+
+touchArea.addEventListener("touchend",function(e){
+    e.preventDefault
+    touchArea.style.backgroundColor="gray";
+    touchArea.textContent="Touch End!";
+});
+
+touchArea.addEventListener("touchcancel",function(e){
+    e.preventDefault
+    touchArea.style.backgroundColor="red";
+    touchArea.textContent="Touch Cancel!";
+});
